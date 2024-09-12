85413f53347e46da46931cf2567e7e72dc3491fc
Patrick Oberdorf <p.oberdorf@mobilistics.de>
2024-09-12 14:58:52 +0200

    update to newest ubuntu version

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,14 @@
-FROM ubuntu:focal
-MAINTAINER Patrick Oberdorf <patrick@oberdorf.net>
+FROM ubuntu:24.04
 
 RUN apt-get update && apt-get install -y \
-	openssh-server \
-	mcrypt \
-	&& mkdir /var/run/sshd \
-	&& chmod 0755 /var/run/sshd \
-	&& mkdir -p /data/incoming \
-	&& apt-get clean \
-	&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* \
-	&& mkdir /ssh/
+  openssh-server \
+  mcrypt \
+  && mkdir /var/run/sshd \
+  && chmod 0755 /var/run/sshd \
+  && mkdir -p /data/incoming \
+  && apt-get clean \
+  && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* \
+  && mkdir /ssh/
 
 ADD start.sh /usr/local/bin/start.sh
 ADD sshd_config /etc/ssh/sshd_config
--- start.sh
+++ start.sh
@@ -14,30 +14,30 @@ for type in rsa dsa ecdsa ed25519; do
   ln -sf "/ssh/ssh_host_${type}_key.pub" "/etc/ssh/ssh_host_${type}_key.pub"
 done
 
-if ( id ${USER} ); then
-    echo "INFO: User ${USER} already exists"
+if (id ${USER}); then
+  echo "INFO: User ${USER} already exists"
 else
-    echo "INFO: User ${USER} does not exists, we create it"
-    ENC_PASS=$(perl -e 'print crypt($ARGV[0], "password")' ${PASS})
+  echo "INFO: User ${USER} does not exists, we create it"
+  ENC_PASS=$(perl -e 'print crypt($ARGV[0], "password")' ${PASS})
 
-    GET_USER_BY_ID=$(cat /etc/passwd | grep ${USER_ID} | head -n1 | awk -F: '{ print $1 }')
-    deluser ${GET_USER_BY_ID}
+  GET_USER_BY_ID=$(cat /etc/passwd | grep ${USER_ID} | head -n1 | awk -F: '{ print $1 }')
+  deluser ${GET_USER_BY_ID}
 
-    GET_GROUP_BY_ID=$(cat /etc/group | grep ${GROUP_ID} | head -n1 | awk -F: '{ print $1 }')
-    delgroup ${GET_GROUP_BY_ID}
+  GET_GROUP_BY_ID=$(cat /etc/group | grep ${GROUP_ID} | head -n1 | awk -F: '{ print $1 }')
+  delgroup ${GET_GROUP_BY_ID}
 
-    addgroup --gid ${GROUP_ID} sftp-only
+  addgroup --gid ${GROUP_ID} sftp-only
 
-    useradd -d /data -m -g sftp-only -p ${ENC_PASS} -u ${USER_ID} -s /bin/false ${USER}
-    usermod -aG sftp-only ${USER}
+  useradd -d /data -m -g sftp-only -p ${ENC_PASS} -u ${USER_ID} -s /bin/false ${USER}
+  usermod -aG sftp-only ${USER}
 
-    chown ${USER_ID}:${GROUP_ID} /data/incoming
+  chown ${USER_ID}:${GROUP_ID} /data/incoming
 
-    if (  ! -e /data/.ssh/authorized_keys && ! -z "$PUBKEY" ); then
-        mkdir /data/.ssh
-        touch /data/.ssh/authorized_keys
-        echo ${PUBKEY} >> /data/.ssh/authorized_keys
-    fi
+  if [ ! -e /data/.ssh/authorized_keys ] && [ -n "$PUBKEY" ]; then
+    mkdir /data/.ssh
+    touch /data/.ssh/authorized_keys
+    echo ${PUBKEY} >>/data/.ssh/authorized_keys
+  fi
 fi
 
 exec /usr/sbin/sshd -D -e
